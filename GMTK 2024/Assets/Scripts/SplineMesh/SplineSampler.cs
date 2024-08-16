a7b3c0764c5be4f9bdd68683feb5065485e6a834
Franziska Schneider <franz@Freund-Rechner.local>
2024-08-17 00:32:22 +0200

    Add Simple Spline Mesh Generation

---
+++ GMTK 2024/Assets/Scripts/SplineMesh/SplineSampler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine.Splines;
+using UnityEngine;
+using UnityEditor;
+
+namespace Game
+{
+    public class SplineSampler : MonoBehaviour
+    {
+        
+        [SerializeField]
+        private SplineContainer _splineContainer;
+        // Start is called before the first frame update
+        [SerializeField]
+        private int _splineIndex;
+        [SerializeField]
+        private float _bodyWidth = 1f;
+
+        float3 position;
+        float3 forward;
+        float3 normal;
+
+        public void SampleSplineVertices(float t, out Vector3 left, out Vector3 right)
+        {
+            _splineContainer.Evaluate(_splineIndex, t, out position, out forward, out normal);
+            float3 distance = Vector3.Cross(forward, normal).normalized * _bodyWidth;
+            right = position + distance;
+            left = position - distance;
+        }
+
+    }
+}
